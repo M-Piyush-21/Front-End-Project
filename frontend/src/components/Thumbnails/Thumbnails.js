8aa485537597c62d01cec2729033641b1e3b691f
Mahajan Piyush <2023.mahajanp@isu.ac.in>
2025-03-17 01:09:18 +0530

    fixed the count of data

---
+++ frontend/src/components/Thumbnails/Thumbnails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Thumbnails({foods}) {
+  return (
+    
+    <div>
+      Thumbnails{foods.length}
+    </div>
+  )
+}
--- frontend/src/pages/Home/Homepage.js
+++ frontend/src/pages/Home/Homepage.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
+import { getAll } from '../../services/foodservice';
+import Thumbnails from '../../components/Thumbnails/Thumbnails';
 const initialState = {foods:[]};
 const reducer =(state,action)=>{
   switch(action.type){
     case 'FOODS_LOADED':
-      return {...state,foods:action.paylaod};
+      return {...state,foods:action.payload};
     default:
       return state;
   }
 };
 export default function Homepage() {
+  const [state,dispatch] =  useReducer(reducer,initialState);
+  const {foods} =state;
+  
+  useEffect(()=>{ getAll().then((foods) => {dispatch({type:'FOODS_LOADED',payload: foods}) })
+  },[])
   return (
-    <div>
-      Homepage
-    </div>
+    <>
+      <Thumbnails foods={foods}/>
+    </>
   )
 }
